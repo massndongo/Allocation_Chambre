1a2d5b9b695584220b889ca43e5e8cbfcadd651b
Massam NDONGO <massndongo86@gmail.com>
2020-06-23 03:49:14 +0000

    POO

---
+++ models/EtudiantNonBoursier.php
@@ -0,0 +1,11 @@
+<?php
+class EtudiantLoger extends User{
+    
+    public function __construct($row=null){
+
+    }
+    //Redefinition
+    public function setProfil($profil){
+        
+    }
+}
